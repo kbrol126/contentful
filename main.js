18a2b9fe7d0aea3ac9cc2823d32d6420fd8b0a39
kbrol126 <nahu_sank06@hotmail.com>
2022-08-18 21:11:33 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
+function mostrarResultados(resultados) {
+  const contenedor = document.querySelector(".contenedor");
+  const template = document.querySelector("#cards");
+  const arrayConUrls = resultados.includes.Asset.map((x) => x.fields.file.url);
+
+  for (const r of resultados.items) {
+    const titleEl = template.content.querySelector(".title-card");
+    titleEl.textContent = r.fields.title;
+    const descriptionEl = template.content.querySelector(".p-card");
+    descriptionEl.textContent = r.fields.descripcion;
+    const urlEl = template.content.querySelector(".link");
+    urlEl.href = r.fields.url;
+    const imgEl = template.content.querySelector(".img-card");
+    imgEl.src = arrayConUrls[0];
+    const clonar = document.importNode(template.content, true);
+    contenedor.appendChild(clonar);
+  }
+}
+
 function main() {
   fetch(
-    "https://cdn.contentful.com/spaces/7dmr0uwu807a/environments/master/entries/1P0qLFW046Z54xWREw5kjj?access_token=BtX96HIXaQgEwTZIIp3ornmYOlSDW3miO7WenL3YLC8"
+    "https://cdn.contentful.com/spaces/7dmr0uwu807a/environments/master/entries?access_token=psrVG_69pbSZ7C7d7jtQzABmYvEzQYILehhustser-w"
   )
     .then((response) => response.json())
     .then((data) => mostrarResultados(data));
-}
-
-function mostrarResultados(resultados) {
-  console.log(resultados.fields);
 }
 
 main();
